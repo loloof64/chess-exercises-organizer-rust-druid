52f6995b39fda440dfdbf4e113cc02adf19c73ed
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-01-08 23:59:12 +0100

    Drawing chess pieces
    in chess board widget.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Chess exercises organizer
 
 Load your chess pgn files, and train yourself against your computer.
+
+Piece sets
+----------
+
+Original pieces vectors definitions from CBurnett and found on [Wikimedia commons](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces).
+
--- src/graphic/chess_board.rs
+++ src/graphic/chess_board.rs
@@ -1,6 +1,10 @@
 use druid::text::{ArcStr, FontDescriptor, TextLayout};
 use druid::widget::prelude::*;
-use druid::{Color, FontFamily, FontWeight, Rect};
+use druid::{widget::SvgData, Color, FontFamily, FontWeight, Rect, Affine};
+use log::error;
+
+use pleco::core::{sq::SQ, Piece};
+use pleco::Board;
 
 #[derive(Data, Clone, Debug)]
 pub struct ChessBoardData {
@@ -64,7 +68,11 @@ impl ChessBoard {
 
             let color = Color::rgb(255.0, 199.0, 0.0);
             let mut label = TextLayout::<ArcStr>::from_text(current_str);
-            label.set_font(FontDescriptor::new(FontFamily::SANS_SERIF).with_size(font_size).with_weight(FontWeight::BOLD));
+            label.set_font(
+                FontDescriptor::new(FontFamily::SANS_SERIF)
+                    .with_size(font_size)
+                    .with_weight(FontWeight::BOLD),
+            );
             label.set_text_color(color);
             label.rebuild_if_needed(ctx.text(), env);
 
@@ -82,7 +90,11 @@ impl ChessBoard {
 
             let color = Color::rgb(255.0, 199.0, 0.0);
             let mut label = TextLayout::<ArcStr>::from_text(current_str);
-            label.set_font(FontDescriptor::new(FontFamily::SANS_SERIF).with_size(font_size).with_weight(FontWeight::BOLD));
+            label.set_font(
+                FontDescriptor::new(FontFamily::SANS_SERIF)
+                    .with_size(font_size)
+                    .with_weight(FontWeight::BOLD),
+            );
             label.set_text_color(color);
             label.rebuild_if_needed(ctx.text(), env);
 
@@ -90,6 +102,64 @@ impl ChessBoard {
                 label.draw(ctx, (x1, y));
                 label.draw(ctx, (x2, y));
             });
+        }
+    }
+
+    fn draw_pieces(&self, ctx: &mut PaintCtx, data: &ChessBoardData) {
+        let total_size = ctx.size().width;
+        let cells_size = total_size * 0.1111;
+        let position = data.position.as_str();
+
+        let board = Board::from_fen(position).expect(
+            format!(
+                "Could not create board logic from current position {} !",
+                position
+            )
+            .as_str(),
+        );
+        for row in 0..8 {
+            let rank = 7-row;
+            for col in 0..8 {
+                let file = col;
+
+                let square = SQ((file + 8 * rank) as u8);
+                let piece = board.piece_at_sq(square);
+
+                let piece_image_path = match piece {
+                    Piece::WhitePawn => Some(include_str!("../merida/wP.svg")),
+                    Piece::WhiteKnight =>  Some(include_str!("../merida/wN.svg")),
+                    Piece::WhiteBishop =>  Some(include_str!("../merida/wB.svg")),
+                    Piece::WhiteRook =>  Some(include_str!("../merida/wR.svg")),
+                    Piece::WhiteQueen =>  Some(include_str!("../merida/wQ.svg")),
+                    Piece::WhiteKing =>  Some(include_str!("../merida/wK.svg")),
+                    Piece::BlackPawn =>  Some(include_str!("../merida/bP.svg")),
+                    Piece::BlackKnight =>  Some(include_str!("../merida/bN.svg")),
+                    Piece::BlackBishop =>  Some(include_str!("../merida/bB.svg")),
+                    Piece::BlackRook =>  Some(include_str!("../merida/bR.svg")),
+                    Piece::BlackQueen => Some(include_str! ("../merida/bQ.svg")),
+                    Piece::BlackKing =>  Some(include_str!("../merida/bK.svg")),
+                    Piece::None => None,
+                };
+                if let Some(piece_image_path) = piece_image_path {
+                    let piece_svg_data = match piece_image_path.parse::<SvgData>(){
+                        Ok(svg) => svg,
+                        Err(err) => {
+                            error!("{}", err);
+                            error!("Using an empty SVG instead of {}.", piece_image_path);
+                            SvgData::default()
+                        }
+                    };
+                    
+                    let ratio = (cells_size as f64) / 45_f64;
+                    let x = cells_size * (0.5 + (col as f64));
+                    let y = cells_size * (0.5 + (row as f64));
+                    let affine_matrix =Affine::translate((x, y)) * Affine::scale(ratio);
+
+                    ctx.with_save(|ctx| {
+                        piece_svg_data.to_piet(affine_matrix, ctx);
+                    });
+                }
+            }
         }
     }
 }
@@ -141,9 +211,10 @@ impl Widget<ChessBoardData> for ChessBoard {
         bc.constrain(Size::new(new_common_size, new_common_size))
     }
 
-    fn paint(&mut self, ctx: &mut PaintCtx, _data: &ChessBoardData, env: &Env) {
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &ChessBoardData, env: &Env) {
         self.draw_background(ctx);
         self.draw_cells(ctx);
         self.draw_coordinates(ctx, env);
+        self.draw_pieces(ctx, data);
     }
 }
