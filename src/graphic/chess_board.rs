bc4a35f762433ebf79c12e5b949da34690d2d581
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-01-08 18:19:08 +0100

    Drawing chess board cells

--- src/graphic/chess_board.rs
+++ src/graphic/chess_board.rs
@@ -1,5 +1,5 @@
 use druid::widget::prelude::*;
-use druid::Color;
+use druid::{Color, Rect};
 
 #[derive(Data, Clone, Debug)]
 pub struct ChessBoardData {
@@ -19,6 +19,28 @@ pub struct ChessBoard;
 impl ChessBoard {
     pub fn new() -> Self {
         Self {}
+    }
+
+    fn draw_background(&self, ctx: &mut PaintCtx) {
+        let size = ctx.size();
+        let rect = size.to_rect();
+        ctx.fill(rect, &Color::rgb8(214, 59, 96));
+    }
+
+    fn draw_cells(&self, ctx: &mut PaintCtx) {
+        let total_size = ctx.size().width;
+        let cells_size = total_size * 0.1111;
+        for row in 0..8 {
+            for col in 0..8 {
+                let is_white_cell = (row+col) % 2 > 0;
+                let color = if is_white_cell {Color::rgb8(255, 206, 158)} else  {Color::rgb8(209, 139, 71)};
+                let x = cells_size * (0.5 + (col as f64));
+                let y = cells_size * (7.5 - (row as f64));
+
+                let rect = Rect::new(x, y, x+(cells_size as f64), y + (cells_size as f64));
+                ctx.fill(rect, &color);
+            }
+        }
     }
 }
 
@@ -70,8 +92,7 @@ impl Widget<ChessBoardData> for ChessBoard {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, _data: &ChessBoardData, _env: &Env) {
-        let size = ctx.size();
-        let rect = size.to_rect();
-        ctx.fill(rect, &Color::rgb8(214, 59, 96));
+        self.draw_background(ctx);
+        self.draw_cells(ctx);
     }
 }
